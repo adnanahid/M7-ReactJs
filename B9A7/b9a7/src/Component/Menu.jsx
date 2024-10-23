c222d46dd3a2d6ab2dfb4f02cb26684b431dc976
adnanahid <sikuahamed@gmail.com>
2024-10-23 10:52:11 +0600

    app.css removed

--- B9A7/b9a7/src/Component/Menu.jsx
+++ B9A7/b9a7/src/Component/Menu.jsx
@@ -15,15 +15,15 @@ const Menu = ({ menuinfo }) => {
       <h2 className="text-xl font-semibold text-start py-3">
         Ingredients: {menuinfo.ingredients.length}
       </h2>
-      <ul className="pb-5 text-gray-600">
+      <ul className="pb-5 text-gray-600 ml-8">
         {menuinfo.ingredients.map((ingredient, index) => (
-          <li key={index} className="text-start pl-3 pb-1">
+          <li key={index} className="list-disc text-start pb-1">
             {ingredient}
           </li>
         ))}
       </ul>
       <hr />
-      <div className="flex justify-between py-5 text-gray-600"><span>Preparing time: {menuinfo.preparing_time}</span><span>{menuinfo.calories}</span></div>
+      <div className="flex justify-between py-5 text-gray-600"><span><i className="fa-solid fa-stopwatch"></i> {menuinfo.preparing_time}</span><span><i className="fa-brands fa-gripfire"></i> {menuinfo.calories}</span></div>
       <button className="btn text-white font-bold bg-green-500 rounded-3xl">Want to coock</button>
     </div>
   );
